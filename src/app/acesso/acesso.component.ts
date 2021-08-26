dfe8e83a46ac0bdfe7d657a683dd1d9ab9a6bb7d
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-08-26 19:43:05 -0300

    Adicionada animação para o componente de login

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -17,12 +17,25 @@ import { Component, OnInit } from '@angular/core';
         }),
         animate('500ms 0s ease-in-out') //duração, delay e aceleração
       ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(50px, 0)'
+        }),
+        animate('500ms 0s ease-in-out') //duração, delay e aceleração
+      ])
     ])
   ]
 })
 export class AcessoComponent implements OnInit {
 
   public estadoBanner: string = 'criado'
+  public estadoPainel: string = 'criado'
 
   constructor() { }
 
