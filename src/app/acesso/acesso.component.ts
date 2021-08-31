ccf8f93f16fa6e1ed630e72490411bba45e7249f
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-08-30 23:17:47 -0300

    Adição de keyframes na animação

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger, keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -27,7 +27,17 @@ import { Component, OnInit } from '@angular/core';
           opacity: 0,
           transform: 'translate(50px, 0)'
         }),
-        animate('500ms 0s ease-in-out') //duração, delay e aceleração
+        animate('1.5s 0s ease-in-out', keyframes([
+          style({offset: 0.15, opacity: 1, transform: 'translateX(0)'}),
+          style({offset: 0.86, opacity: 1, transform: 'translateX(0)'}),
+          style({offset: 0.88, opacity: 1, transform: 'translateY(-30px)'}),
+          style({offset: 0.90, opacity: 1, transform: 'translateY(30px)'}),
+          style({offset: 0.92, opacity: 1, transform: 'translateY(-20px)'}),
+          style({offset: 0.94, opacity: 1, transform: 'translateY(20px)'}),
+          style({offset: 0.96, opacity: 1, transform: 'translateY(-10px)'}),
+          style({offset: 0.98, opacity: 1, transform: 'translateY(10px)'}),
+          style({offset: 1, opacity: 1, transform: 'translateY(-5px)'})
+        ])) //duração, delay e aceleração
       ])
     ])
   ]
@@ -37,7 +47,7 @@ export class AcessoComponent implements OnInit {
   public estadoBanner: string = 'criado'
   public estadoPainel: string = 'criado'
 
-  public cadastro: boolean = true
+  public cadastro: boolean = false
 
   constructor() { }
 
