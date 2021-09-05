97699a7cc9f59fdff0aebe0dfdf168349593bb29
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-09-05 08:27:48 -0300

    Configuração do firebase na aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from '@firebase/app';
+
+// import Firebase Authentication (optional)
+import '@firebase/auth';
+
+// import Firebase Realtime Database (optional)
+import '@firebase/database';
+
+// import Cloud Firestore (optional)
+import '@firebase/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'instaclone';
+
+  ngOnInit(): void {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDPhEnCKweNeQoH8rinRRsBPLyHduz6ukM",
+      authDomain: "jta-instaclone-6f433.firebaseapp.com",
+      projectId: "jta-instaclone-6f433",
+      storageBucket: "jta-instaclone-6f433.appspot.com",
+      messagingSenderId: "109352664265",
+      appId: "1:109352664265:web:99c01ac9cbcbad4f0be118"
+    };
+    firebase.initializeApp(firebaseConfig)
+  }
 }
