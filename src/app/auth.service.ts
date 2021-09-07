3bfb866333f90712c38e60525f3051f581686728
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-09-06 23:51:26 -0300

    Início da inserção de usuários no banco

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,24 @@
 import { Usuario } from "./acesso/usuario.model";
+import * as firebase from '@firebase/app';
+
+// import Firebase Authentication (optional)
+import * as fireAuth from '@firebase/auth';
+
+// import Firebase Realtime Database (optional)
+import * as fireData from '@firebase/database';
+
+// import Cloud Firestore (optional)
+import * as fireStore from '@firebase/firestore';
 
 export class Auth {
     public cadastrarUsuario(usuario: Usuario): void {
         console.log('Chegou no serviço: ', usuario);
+        const auth = fireAuth.getAuth();
+        fireAuth.createUserWithEmailAndPassword(auth, usuario.email, usuario.senha)
+            .then((resposta: any) => {
+                console.log(resposta);
+            }).catch((error: Error) => {
+                console.log(error);
+            });
     }
 }
