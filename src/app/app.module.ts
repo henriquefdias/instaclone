9c623d740a8699026c90bb8481ae9940fa12ca83
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-09-12 12:07:24 -0300

    Criação das rotas com RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { ROUTES } from "./app.routes";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +30,8 @@ import { PublicacoesComponent } from './home/publicacoes/publicacoes.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [Auth],
   bootstrap: [AppComponent]
