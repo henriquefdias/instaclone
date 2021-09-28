4bea7ebbd9402d7b3e08a24bcefa1d46ab23f9d8
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-09-28 10:11:13 -0300

    Inserindo informações no firebase

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -1,5 +1,22 @@
+import * as firebase from '@firebase/app';
+
+// import Firebase Authentication (optional)
+import * as fireAuth from '@firebase/auth';
+
+// import Firebase Realtime Database (optional)
+import * as fireData from '@firebase/database';
+
+// import Cloud Firestore (optional)
+import * as fireStore from '@firebase/firestore';
+
 export class Bd {
-    public publicar(): void {
+    public publicar(publicacao: any): void {
+
+        const DATA = fireData.getDatabase();
+        let refData: any = fireData.ref(DATA, `publicacoes/${btoa(publicacao.email)}`);
+        fireData.push(refData, {titulo: publicacao.titulo})
+
+
         console.log("Chegou no serviço bd");
     }
 }
