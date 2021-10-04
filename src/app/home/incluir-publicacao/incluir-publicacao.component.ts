aec6d2597ceae95a22da19453c82be39db172b14
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-10-04 13:34:45 -0300

    Feedback visual do progresso de upload

--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -16,6 +16,9 @@ export class IncluirPublicacaoComponent implements OnInit {
   public email: any = ''
   private imagem: any
 
+  public progressoPublicacao: string = 'pendente'
+  public porcentagemUpload: number = 0
+
   public formulario: FormGroup = new FormGroup({
     'titulo': new FormControl(null)
   })
@@ -44,14 +47,16 @@ export class IncluirPublicacaoComponent implements OnInit {
     continua.next(true)
 
     acompanhamentoUpload
-      .pipe(
-        takeUntil(continua)
-      )
+      .pipe(takeUntil(continua))
       .subscribe(() => {
       console.log(this.progresso.status);
       console.log(this.progresso.estado);
+      this.progressoPublicacao = 'andamento'
+
+      this.porcentagemUpload = Math.round((this.progresso.estado.bytesTransferred / this.progresso.estado.totalBytes) * 100)
 
       if (this.progresso.status === 'concluido') {
+        this.progressoPublicacao = 'concluido'
         continua.next(false)
       }
     })
