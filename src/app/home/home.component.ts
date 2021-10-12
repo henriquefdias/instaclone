fcf907dea787d5609084d1220df23bc5792a5e1e
henriquefdias <60013916+henriquefdias@users.noreply.github.com>
2021-10-12 12:10:51 -0300

    Final da atualização da timeline ao inserir post

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Auth } from '../auth.service';
 
 @Component({
@@ -7,6 +7,8 @@ import { Auth } from '../auth.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  @ViewChild('publicacoes') public publicacoes: any
 
   constructor(private auth: Auth) { }
 
@@ -18,7 +20,7 @@ export class HomeComponent implements OnInit {
   }
 
   public atualizarTimeline() {
-    console.log(`chegou aqui`);
+    this.publicacoes.atualizarTimeline()
   }
 
 }
